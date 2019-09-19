95e3f21e41fb88be349bdb7c3431b45ebc0c8138
Alex Beloshevsky <alexbeloshevsky@gmail.com>
2019-09-19 18:14:11 +0300

    connected the local database

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
-
 const app = express();
-
 const PORT = process.env.PORT || 5000;
+const connectDB = require('./config/db');
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
